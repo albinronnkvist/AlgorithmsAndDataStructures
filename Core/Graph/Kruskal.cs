45bd783d392a45679e713a2122b55c592631d50f
Albin <albinronnkvist@protonmail.com>
2024-08-21 16:57:12 +0200

    Add empty Kruskal

---
+++ Core/Graph/Kruskal.cs
@@ -0,0 +1,9 @@
+namespace Albin.AlgorithmsAndDataStructures.Core.Graph;
+
+public static class Kruskal
+{
+    public static Dictionary<int, List<(int, int)>> Execute(Dictionary<int, List<(int, int)>> graph)
+    {
+        throw new NotImplementedException();
+    }
+}
