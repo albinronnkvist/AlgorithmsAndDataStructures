ae37d4a86564c81d3288313d1814e93eb75426d8
Albin <albinronnkvist@protonmail.com>
2024-08-21 16:59:18 +0200

    Remove Graphs

--- Core/Graph/Kruskal.cs
+++
@@ -1,9 +0,0 @@
-namespace Albin.AlgorithmsAndDataStructures.Core.Graph;
-
-public static class Kruskal
-{
-    public static Dictionary<int, List<(int, int)>> Execute(Dictionary<int, List<(int, int)>> graph)
-    {
-        throw new NotImplementedException();
-    }
-}
